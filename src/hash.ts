47f2d5f1ca334e73cd36709ca47f69d5d15fe8c5
Jasper Palfree <well.caffeinated@gmail.com>
2024-11-07 15:16:07 -0700

    chore: cleanup

--- src/hash.ts
+++ src/hash.ts
@@ -1,31 +1,31 @@
-import jsSHA from 'jssha';
+import jsSHA from 'jssha'
 
-type OutputFormat = 'UINT8ARRAY' | 'HEX' | 'B64' | 'ARRAYBUFFER';
+type OutputFormat = 'UINT8ARRAY' | 'HEX' | 'B64' | 'ARRAYBUFFER'
 
 function hasher(algorithm: any): {
-  (iterable: Iterable<Uint8Array> | AsyncIterable<Uint8Array>, outputFormat: 'UINT8ARRAY', options?: any): Promise<Uint8Array>;
-  (iterable: Iterable<Uint8Array> | AsyncIterable<Uint8Array>, outputFormat: 'HEX' | 'B64', options?: any): Promise<string>;
-  (iterable: Iterable<Uint8Array> | AsyncIterable<Uint8Array>, outputFormat: 'ARRAYBUFFER', options?: any): Promise<ArrayBuffer>;
+  (iterable: Iterable<Uint8Array> | AsyncIterable<Uint8Array>, outputFormat: 'UINT8ARRAY', options?: any): Promise<Uint8Array>
+  (iterable: Iterable<Uint8Array> | AsyncIterable<Uint8Array>, outputFormat: 'HEX' | 'B64', options?: any): Promise<string>
+  (iterable: Iterable<Uint8Array> | AsyncIterable<Uint8Array>, outputFormat: 'ARRAYBUFFER', options?: any): Promise<ArrayBuffer>
 } {
   return async (
     iterable: Iterable<Uint8Array> | AsyncIterable<Uint8Array>,
     outputFormat: OutputFormat = 'UINT8ARRAY',
     options?: any
   ): Promise<any> => {
-    const sha = new jsSHA(algorithm, 'UINT8ARRAY', options);
+    const sha = new jsSHA(algorithm, 'UINT8ARRAY', options)
     for await (const chunk of iterable) {
-      sha.update(chunk);
+      sha.update(chunk)
     }
     // @ts-ignore
-    return sha.getHash(outputFormat, options);
-  };
+    return sha.getHash(outputFormat, options)
+  }
 }
 
 /**
  * @ignore
  */
-export const shake256 = hasher('SHAKE256');
+export const shake256 = hasher('SHAKE256')
 /**
  * @ignore
  */
-export const sha3512 = hasher('SHA3-512');
+export const sha3512 = hasher('SHA3-512')
